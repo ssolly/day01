eb2a8567fe269a953ffe3cdd544ae9818c1611da
Choi.SY <sollly0829@gmail.com>
2021-07-23 11:19:11 +0900

    java 주석

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -4,8 +4,13 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
+		//sysout(ctrl+space)는 괄호 안의 내용을 출력하는 기능
+		// ;(세미콜론)은 문장의 끝을 알리는 용도
+		
 		System.out.println("안녕하세요");
 		System.out.println("자바 시간입니다");
 		
+		System.out.print("안녕하세요");
+		System.out.print("자바 시간입니다");
 	}
 }
