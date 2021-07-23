336e87565fdeacfb60f806be7d1547227ac65f97
Choi.SY <sollly0829@gmail.com>
2021-07-23 12:49:32 +0900

    Ex02 문자열 관련 기능

--- src/day01/Ex02.java
+++ src/day01/Ex02.java
@@ -6,9 +6,17 @@ public class Ex02 {
 		/*
 		 escape문자자
 		  - \n : enter(엔터)와 동일한 역할
-		  - \t : tab(탭) 크기만큼 오른쪽으로 이동
+		  - \t : tab(탭) 크기만큼 오른쪽으로 이동(window-8칸)
 		  - \" : "(큰따옴표)를 출력할 때
 		  - \\ : \(역슬러쉬) 출력할 때 
 		 */
+		
+		/*
+		 - " " : 쌍따옴표로 묶인 값은 문자열로 인식한다
+		 */
+		System.out.println("H\ne\nl\nl\no\n Java");
+		System.out.println("1\t1234567\t12345678\t12");
+		System.out.println("test \"123\" 456");
+		System.out.println("C:\\Users\\springcloud\\java\\workspace\\day01");
 	}
 }
