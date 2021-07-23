688108007cc513ff60c3c72bf27dfa45ed8d69d7
Choi.SY <sollly0829@gmail.com>
2021-07-23 13:02:22 +0900

    Ex03 문자열과 숫자 연산

--- src/day01/Ex03.java
+++ src/day01/Ex03.java
@@ -1,5 +1,14 @@
 package day01;
 
 public class Ex03 {
-
+	//class 만들 때 첫글자는 무조건 대문자
+	
+	public static void main(String[] args) {
+		
+		System.out.println(100+200);
+		System.out.println("문자열"+100);
+		System.out.println("문자열"+100+500);
+		System.out.println("문자열"+(100+100));
+		System.out.println("100"+100);
+	}
 }
