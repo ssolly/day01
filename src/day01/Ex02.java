640af4e7e69565f1bd67c4b5b7130db1f51e67d1
Choi.SY <sollly0829@gmail.com>
2021-07-23 11:31:49 +0900

    Ex02 생성

---
+++ src/day01/Ex02.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		/*
+		 escape문자자
+		  - \n : enter(엔터)와 동일한 역할
+		  - \t : tab(탭) 크기만큼 오른쪽으로 이동
+		  - \" : "(큰따옴표)를 출력할 때
+		  - \\ : \(역슬러쉬) 출력할 때 
+		 */
+	}
+}
